c4a36794da0f3dcca2c4944eca53529a86e35d80
nayeemmehedi <boyneel79@gmail.com>
2022-11-18 08:24:31 -0800

    added 2nd callback example

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
+// function firseFuntion(value, callBack) {
+//   console.log("hello man");
+
+//   callBack(value);
+// }
+
+// function callMe(value){
+//     console.log("call me " + value)
+// }
+
+// firseFuntion("nayeem",callMe)
+
+// 2nd example
+
 function firseFuntion(value, callBack) {
-  console.log("hello man");
-
-  callBack(value);
-}
-
-function callMe(value){
+    console.log("hello man");
+  
+    callBack(value);
+  }
+  
+ 
+  
+  firseFuntion("nayeem", value=>{
     console.log("call me " + value)
-}
-
-firseFuntion("nayeem",callMe)
+})
