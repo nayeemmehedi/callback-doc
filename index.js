55061fa5a804954f6f6a9afb0c63a243e99e2186
nayeemmehedi <boyneel79@gmail.com>
2022-11-18 09:16:05 -0800

    done work

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-git branch nayeem
-git branch -a 
-git checkout nayeem
+// git branch nayeem
+// git branch -a 
+// git checkout nayeem
+
+function nayeem(name,callback){
+    console.log("helo",name)
+    callback(name)
+}
+
+nayeem("NAYEEM",(v)=>{
+    console.log("done",v )
+})
